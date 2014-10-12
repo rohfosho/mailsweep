e2c549052af983168b3e82768e36c1f06d40f952
Joshua Liu <hockeyhelp6830@gmail.com>
2014-10-11 23:22:47 -0400

    lol

--- models/User.js
+++ models/User.js
@@ -4,18 +4,7 @@ var crypto = require('crypto');
 
 var userSchema = new mongoose.Schema({
   email: { type: String, unique: true, lowercase: true },
-  password: String,
-
-  google: String,
-  instagram: String,
-  tokens: Array,
-
-  profile: {
-    name: { type: String, default: '' },
-    gender: { type: String, default: '' },
-    location: { type: String, default: '' },
-    website: { type: String, default: '' },
-    picture: { type: String, default: '' }
+  phone: String
   },
 
   resetPasswordToken: String,
